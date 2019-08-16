6e6bcf5878da73e769857c8e428cff8e71f00f19
santhosh205 <santhoshdasari205@gmail.com>
2019-08-17 02:40:28 +0530

    docs(components): added basic description

--- src/app/modules/drag-n-drop/drag-n-drop.component.ts
+++ src/app/modules/drag-n-drop/drag-n-drop.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
-  selector: 'app-drag-n-drop',
+  selector: 'drag-n-drop',
   templateUrl: './drag-n-drop.component.html',
   styleUrls: ['./drag-n-drop.component.scss']
 })
